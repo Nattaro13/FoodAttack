e40d5ab77d608f845a0f21adae5d2ee042db8c5f
XueHui <nattaro13@gmail.com>
2015-06-29 14:17:45 +0800

    Added "Search" functionality to the ShopList
    1. Updated Android Manifest to support Search
    2. Updated ShopList.java to support search
    3. Created SearchShopList.java to handle searches

--- app/src/main/java/com/foodattack/foodattack/SearchShopList.java
+++ app/src/main/java/com/foodattack/foodattack/SearchShopList.java
@@ -1,19 +1,30 @@
 package com.foodattack.foodattack;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SimpleCursorAdapter;
+
+import com.foodattack.foodattack.db.ShopListDBHelper;
+import com.foodattack.foodattack.db.ShopListContract;
 
 /**
  * Created by Xue Hui on 29/6/2015.
  */
-public class SearchShopList extends Activity {
+public class SearchShopList extends ListActivity {
+    private ShopListDBHelper helper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_shop_list);
+        Log.d("Entered Searchable","Searchable");
         handleIntent(getIntent());
     }
 
@@ -35,6 +46,44 @@ public class SearchShopList extends Activity {
 
     private void showResults(String query) {
         // Query your data set and show results
-        // ...
+        helper = new ShopListDBHelper(SearchShopList.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+
+        String whereClause = String.format("%s = ?",ShopListContract.Columns.ITEM_NAME);
+        //Cursor cursor = sqLiteDatabase.query(
+        //tableName, tableColumns, whereClause, whereArgs, groupBy, having, orderBy);
+        Cursor cursor = sqlDB.query(ShopListContract.TABLE,
+                new String[]{ShopListContract.Columns._ID,
+                        ShopListContract.Columns.ITEM_NAME,
+                        ShopListContract.Columns.ITEM_QTY},
+                whereClause, new String[]{query}, null, null, null);
+
+        SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.shop_list_view,
+                cursor,
+                new String[] { ShopListContract.Columns.ITEM_NAME,
+                        ShopListContract.Columns.ITEM_QTY},
+                new int[] { R.id.ShopList_ItemName_View,R.id.ShopList_ItemQty_View},
+                0
+        );
+        this.setListAdapter(listAdapter);
     }
+
+
+
+    /**
+     * Builds a table of items matching the searchTerm in their name or description
+     */
+    /*
+    public Cursor searchMenuItems(String searchTerm) {
+        helper = new ShopListDBHelper(SearchShopList.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(ShopListContract.TABLE);
+        Cursor c = qb.query(sqlDB, null, "("+ShopListContract.Columns.ITEM_NAME.toString()+" LIKE '%"+searchTerm+"%') " +
+                        "OR ("+ShopListContract.Columns.ITEM_BRAND+" LIKE '%" + searchTerm+"%')",
+                null, null, null, null);
+        return c;
+    }  */
 }
