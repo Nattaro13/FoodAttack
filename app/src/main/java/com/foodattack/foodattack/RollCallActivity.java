fe9808eb948cd537d7099366b647125ed189447a
XueHui <nattaro13@gmail.com>
2015-06-29 11:31:17 +0800

    Resolve button crashes and update main activity UI
    1. Swapped the button positions around for the main activity UI
    2. Update parent class for RollCallActivity and RecipeList to resolve
    button crash

--- app/src/main/java/com/foodattack/foodattack/RollCallActivity.java
+++ app/src/main/java/com/foodattack/foodattack/RollCallActivity.java
@@ -1,12 +1,13 @@
 package com.foodattack.foodattack;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class RollCallActivity extends ActionBarActivity {
+public class RollCallActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
