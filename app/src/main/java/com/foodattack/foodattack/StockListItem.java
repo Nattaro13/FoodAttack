1e8b10215cbe8602802b92ec6ea4facbe1886b5d
garbanzos <chrno_crusade217@hotmail.com>
2015-07-13 17:17:21 +0800

    stocklistitem is no longer a subclass of shoplistitem because static mtds cannot be overriden

--- app/src/main/java/com/foodattack/foodattack/StockListItem.java
+++ app/src/main/java/com/foodattack/foodattack/StockListItem.java
@@ -1,18 +1,47 @@
 package com.foodattack.foodattack;
 
 import com.parse.ParseClassName;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.util.UUID;
 
 /**
  * ShopListItem is a ParseObject that encapsulates items in the stocklist
- * It extends ShopListItem as it has similar attributes as ShopListItem
- * It only has one additional attribute which is restock
  **/
 
 @ParseClassName("StockListItem")
-public class StockListItem extends ShopListItem{
+public class StockListItem extends ParseObject {
+
+    //getters and setters for itemName
+    public String getItemName() {
+        return getString("itemName");
+    }
+
+    public void setItemName(String itemName) {
+        put("itemName", itemName);
+    }
+
+    //getters and setters for itemBrand
+    public String getItemBrand() {
+        return getString("itemBrand");
+    }
+
+    public void setItemBrand(String itemBrand) {
+        put("itemBrand", itemBrand);
+    }
 
     //getters and setters for itemQty
+    // TODO change qty to double when stocklistparse ready
+    public String getItemQty() {
+        return getString("itemQty");
+    }
+
+    public void setItemQty(String itemQty) {
+        put("itemQty", itemQty);
+    }
+
+    //getters and setters for itemRestock
     // TODO change restock to int when stocklistparse ready
     public String getItemRestock() {
         return getString("itemRestock");
@@ -20,6 +49,16 @@ public class StockListItem extends ShopListItem{
 
     public void setItemQty(String itemRestock) {
         put("itemRestock", itemRestock);
+    }
+
+    //TODO what is uuid ?_?
+    public void setUuidString() {
+        UUID uuid = UUID.randomUUID();
+        put("uuid", uuid.toString());
+    }
+
+    public String getUuidString() {
+        return getString("uuid");
     }
 
     public static ParseQuery<StockListItem> getQuery() {
