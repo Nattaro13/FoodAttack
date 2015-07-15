2e0b811de067c2a1d5b5476aabd0d03cc428de68
garbanzos <chrno_crusade217@hotmail.com>
2015-07-16 01:05:30 +0800

    stocklistadapter now displays both item name and qty

--- app/src/main/java/com/foodattack/foodattack/StockListAdapter.java
+++ app/src/main/java/com/foodattack/foodattack/StockListAdapter.java
@@ -15,28 +15,53 @@ public class StockListAdapter extends ArrayAdapter<StockListItem> {
     /**
      * Member Variables
      **/
-    private Context mContext;
     private List<StockListItem> mStockList;
+    private LayoutInflater mInflater;
+
+    /**
+     * Private class that holds textViews
+     */
+    private class ViewHolder {
+        TextView itemNameView;
+        TextView itemQtyView;
+    }
 
     /**
      * Constructor
      **/
     public StockListAdapter(Context context, List<StockListItem> stockList){
-        super(context, R.layout.list_item_stocklist, stockList);
-        mContext = context;
+        super(context, R.layout.list_item_stocklist,stockList);
+        mInflater = LayoutInflater.from(context);
         mStockList = stockList;
     }
 
+    /**
+     * getView
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     * Description: Sets view to display both item name and item qty
+     */
     public View getView(int position, View convertView, ViewGroup parent){
+
+        ViewHolder holder = null;
+
         if(convertView == null){
-            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-            convertView = layoutInflater.inflate(R.layout.list_item_stocklist, null);
+            holder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.list_item_stocklist, null);
+            holder.itemNameView = (TextView) convertView.findViewById(R.id.stocklist_itemNameView);
+            holder.itemQtyView = (TextView) convertView.findViewById(R.id.stocklist_itemQtyView);
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
         }
 
         StockListItem stockItem = mStockList.get(position);
-        TextView itemNameView = (TextView) convertView.findViewById(R.id.stocklist_itemNameView);
-        itemNameView.setText(stockItem.getItemName());
 
+        holder.itemNameView.setText(stockItem.getItemName());
+        holder.itemQtyView.setText(stockItem.getItemQty());
         return convertView;
     }
 }
