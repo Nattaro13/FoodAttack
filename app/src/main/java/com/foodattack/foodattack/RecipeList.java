76a2b6f6fe90d78afb6da5748e34edb733c48382
XueHui <nattaro13@gmail.com>
2015-07-16 18:28:10 +0800

    Added dummy layout for RecipeList and AddRecipe

--- app/src/main/java/com/foodattack/foodattack/RecipeList.java
+++ app/src/main/java/com/foodattack/foodattack/RecipeList.java
@@ -1,10 +1,15 @@
 package com.foodattack.foodattack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class RecipeList extends Activity {
@@ -13,6 +18,22 @@ public class RecipeList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+        final ListView listView = (ListView) findViewById(R.id.recipe_list);
+        String[] recipesList = {"listItem 1", "listItem 2", "listItem 3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.recipe_list_row, R.id.recipe_title, recipesList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                int itemPosition = position;
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+            }
+        });
+
     }
 
     @Override
@@ -31,6 +52,13 @@ public class RecipeList extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+
+        } else if (id == R.id.action_new_recipe) {
+            //Switch interface to the "add recipe activity"
+            Intent intent = new Intent(this, AddRecipe.class);
+            startActivity(intent);
+
             return true;
         }
 
