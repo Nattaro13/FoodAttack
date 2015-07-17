6aa80b53e9d2bce74faef461219448cae8f52c6e
garbanzos <chrno_crusade217@hotmail.com>
2015-07-17 23:46:57 +0800

    created shoplistactivityparse

---
+++ app/src/main/java/com/foodattack/foodattack/ShopListActivityParse.java
@@ -0,0 +1,11 @@
+package com.foodattack.foodattack;
+
+import android.app.Activity;
+
+/**
+ * ShopListActivityParse is the activity that is started when
+ * "Shopping List" of the main activity is clicked
+ **/
+
+public class ShopListActivityParse extends Activity {
+}
