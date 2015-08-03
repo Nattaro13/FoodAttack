d10fe1dd0ce436fde524c6145955d2f861933c27
garbanzos <chrno_crusade217@hotmail.com>
2015-08-03 16:54:25 +0800

    added date picker that opens when focus on restock date edit text (in edit stock item) is changed

--- app/src/main/java/com/foodattack/foodattack/EditStockItemActivity.java
+++ app/src/main/java/com/foodattack/foodattack/EditStockItemActivity.java
@@ -2,6 +2,7 @@ package com.foodattack.foodattack;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,6 +19,10 @@ import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * EditStockItemActiviy is the activity that is started when the user
@@ -69,6 +75,11 @@ public class EditStockItemActivity extends Activity {
         //always get the familyID of user
         mItemFamilyID = intent.getStringExtra("itemFamilyID");
 
+
+        /*onClick restock edit text*/
+        onClickRestockEditText();
+
+        /*onClick save button*/
         mSaveButton = (Button) findViewById(R.id.edit_stock_saveItem);
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +88,38 @@ public class EditStockItemActivity extends Activity {
             }
         });
     }
+
+    /**
+     * onClickRestockEditText
+     * Description: open date picker when the focus on restock date is changed
+     */
+    private void onClickRestockEditText() {
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                //set restock date edit text
+                String myFormat = "dd MMM yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                mItemRestockEditText.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
+        mItemRestockEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    new DatePickerDialog(EditStockItemActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
 
     /**
      * saveItem
@@ -203,5 +246,4 @@ public class EditStockItemActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
